b6faae3e6a2c02d96cf5ef84e898079f8143289e
Aleksej Taratuta <116726052+Liolikmolo@users.noreply.github.com>
2024-03-05 15:03:37 +0300

    main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import { Header } from '@/components/header/header';
+import StartPage from '@/components/start-page/start-page';
 
 export default function Home() {
 	return (
 		<main>
 			<Header />
+			<StartPage />
 		</main>
 	);
 }
