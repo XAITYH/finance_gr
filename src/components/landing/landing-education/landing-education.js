abcb849e95a07377c0da5c34ff3962477409ce08
Aleksej Taratuta <116726052+Liolikmolo@users.noreply.github.com>
2024-04-16 18:32:00 +0300

    fix

--- src/components/landing/landing-education/landing-education.js
+++ src/components/landing/landing-education/landing-education.js
@@ -1,60 +1,67 @@
-import React from "react";
-import styles from "./landing-education.module.css"
-import Image from "next/image";
-import macbook from "../icons/macbook.svg"
-import shadow from "../images/macbookShadow.svg"
-import playButton from "../icons/playButton.svg"
-import arrowUp from "../icons/arrowUp.svg"
-import LandingButton from "../landing-button/landing-button";
+import React from 'react';
+import styles from './landing-education.module.css';
+import Image from 'next/image';
+import macbook from '../icons/macbook.svg';
+import shadow from '../images/macbookShadow.svg';
+import playButton from '../icons/playButton.svg';
+import arrowUp from '../icons/arrowUp.svg';
+import LandingButton from '../landing-button/landing-button';
 
 export default function LandingEducation() {
-    return (
-    <div className={styles.educationWrapper}>
-        <div className={styles.edObjects}>
-            <h2 className={styles.howWeTeach}>Как у нас происходит обучение?</h2>
-            <p className={styles.ourProgram}>
-                Обучение должно быть комфортным. Поэтому мы разработали собственную платформу для обучения программированию. 
-                На ней вы можете не только изучать теорию, но и запускать готовые примеры и даже писать свой собственный код.
-            </p>
-            <div className={styles.video}>
+	return (
+		<div className={styles.educationWrapper}>
+			<div className={styles.edObjects}>
+				<h2 className={styles.howWeTeach}>Как у нас происходит обучение?</h2>
+				<p className={styles.ourProgram}>
+					Обучение должно быть комфортным. Поэтому мы разработали собственную
+					платформу для обучения финансовой грамоте. На ней вы можете не только
+					изучать теорию, но и выполнять интерактивные практические задания.
+				</p>
+				{/* <div className={styles.video}>
                 <Image className={styles.macbook} src={macbook} alt="macbook-image"/>
                 <Image className={styles.macbookShadow} src={shadow} alt="macbook-shadow-image"/>
                 <button className={styles.playButton}>
                     <Image className={styles.playButtonImage} src={playButton} alt="macbook-play-button"/>
                 </button>
                 <h3 className={styles.lookVideo}>Посмотреть видео</h3>
-            </div>
-            <div className={styles.top3}>
-                <div className={styles.top}>
-                    <h1 className={styles.num}>01</h1>
-                    <h2 className={styles.title}>Весь материал разбит на небольшие уроки</h2>
-                    <p className={styles.description1}>
-                        Теория и практика подаются маленькими порциями. 
-                        Так вам будет легче усваивать новые знания.
-                    </p>
-                </div>
-                <div className={styles.top}>
-                    <h1 className={styles.num}>02</h1>
-                    <h2 className={styles.title}>Обучение через практику</h2>
-                    <p className={styles.description2}>
-                        Всё, что вы узнали, вы тут же начинаете применять на практике. 
-                        Вы сразу видите результаты своего труда.
-                    </p>
-                </div>
-                <div className={styles.top}>
-                    <h1 className={styles.num}>03</h1>
-                    <h2 className={styles.title}>Нет ограничений по времени</h2>
-                    <p className={styles.description3}> 
-                        Можно совмещать учёбу с работой и другими дeлами. 
-                        Не нужно выпрашивать академический отпуск, если пришлось сделать перерыв.
-                    </p>
-                </div>
-            </div>
-            <div className={styles.startEducation}>
-                <LandingButton width={249} height={70}>Начать обучение</LandingButton>
-                <p className={styles.testStartText}>Попробуйте, первые уроки бесплатны, но нужно пройти тестирование</p>
-            </div>
-        </div>
-    </div>
-    );
+            </div> */}
+				<div className={styles.top3}>
+					<div className={styles.top}>
+						<h1 className={styles.num}>01</h1>
+						<h2 className={styles.title}>
+							Весь материал разбит на небольшие уроки
+						</h2>
+						<p className={styles.description1}>
+							Теория и практика подаются маленькими порциями. Так вам будет
+							легче усваивать новые знания.
+						</p>
+					</div>
+					<div className={styles.top}>
+						<h1 className={styles.num}>02</h1>
+						<h2 className={styles.title}>Обучение через практику</h2>
+						<p className={styles.description2}>
+							Всё, что вы узнали, вы тут же начинаете применять на практике. Вы
+							сразу видите результаты своего труда.
+						</p>
+					</div>
+					<div className={styles.top}>
+						<h1 className={styles.num}>03</h1>
+						<h2 className={styles.title}>Нет ограничений по времени</h2>
+						<p className={styles.description3}>
+							Можно совмещать учёбу с другими дeлами. Не нужно выпрашивать
+							отпуск, если пришлось сделать перерыв.
+						</p>
+					</div>
+				</div>
+				<div className={styles.startEducation}>
+					<LandingButton width={249} height={70}>
+						Начать обучение
+					</LandingButton>
+					<p className={styles.testStartText}>
+						Попробуйте сами первые уроки, но нужно пройти тестирование
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/landing/landing-knowledge/landing-knowledge.js
+++ src/components/landing/landing-knowledge/landing-knowledge.js
@@ -1,81 +1,110 @@
-import React from "react";
-import styles from "./landing-knowledge.module.css"
-import Image from "next/image";
-import programmer from "../icons/programmer.svg"
-import codeTerminal from "../icons/codeTerminal.svg"
-import sphere from "../icons/sphere.svg"
-import monitorLines from "../icons/monitorLines.svg"
-import calendar from "../icons/calendar.svg"
-import chat from "../icons/chat.svg"
-import coin from "../icons/coin.svg"
-import phoneCard from "../icons/phoneCard.svg"
+import React from 'react';
+import styles from './landing-knowledge.module.css';
+import Image from 'next/image';
+import programmer from '../icons/programmer.svg';
+import codeTerminal from '../icons/codeTerminal.svg';
+import sphere from '../icons/sphere.svg';
+import monitorLines from '../icons/monitorLines.svg';
+import calendar from '../icons/calendar.svg';
+import chat from '../icons/chat.svg';
+import coin from '../icons/coin.svg';
 
 export default function LandingKnowledge() {
-    return (
-        <div className={styles.knowledgeWrapper}>
-            <div className={styles.objects}>
-            <h2 className={styles.whatKnowledge}>Какими знаниями вы овладеете:</h2>
-            <div className={styles.baseKnowledge}>
-                <Image src={programmer} className={styles.programmerImage} alt="programmer-image"/>
-                <div className={styles.bKText}>
-                    <div className={styles.Row}>  
-                            <div className={styles.progLanguage}>Язык программирования Python</div>
-                            <div className={styles.networks}>Сети</div> 
-                    </div>
-                    <div className={styles.Row}>
-                        <div className={styles.bd}>Базы данных</div>
-                        <div className={styles.frameworks}>Фреймворки Flask и Django</div>
-                    </div>
-                    <div className={styles.Row}>
-                        <div className={styles.testing}>Отладка и тестирование</div>
-                        <div className={styles.docker}>Docker</div>
-                        <div className={styles.git}>Git</div>
-                    </div>
-                    <p className={styles.minKnowledge}>Это необходимый минимум для современного backend-разработчика</p>
-                </div>
-            </div>
-            <h2 className={styles.ourEducation}>Обучение у нас – это удобно и результативно</h2>
-                <div className={styles.advantagesTop}>
-                    <div className={styles.advantages}>
-                        <Image className={styles.icons} src={codeTerminal} alt="terminal-image"/>
-                        <h3 className={styles.title}>Огромное количество практики</h3>
-                        <p className={styles.descriptionFirst}>Более 500 самостоятельных заданий и 20 полноценных больших проектов</p>
-                    </div>
-                    <div className={styles.advantages}>
-                        <Image className={styles.icons} src={sphere} alt="sphere-image"/>
-                        <h3 className={styles.title}>Современные методики обучения</h3>
-                        <p className={styles.descriptionSecond}>Спиральное обучение:
-                        погружаемся в материал постепенно, виток за витком</p>
-                    </div>
-                    <div className={styles.advantages}>
-                        <Image className={styles.icons} src={monitorLines} alt="monitor-lines-image"/>
-                        <h3 className={styles.title}>Простое и понятное изложение</h3>
-                        <p className={styles.descriptionThird}>Вместо заумных терминов – примеры из реального мира</p>
-                    </div>
-                    <div className={styles.advantages}>
-                        <Image className={styles.iconsCalendar} src={calendar} alt="calendar-image"/>
-                        <h3 className={styles.title}>Гибкий график занятий</h3>
-                        <p className={styles.descriptionFourth}>Учитесь в любое время в удобном для вас темпе</p>
-                    </div>
-                </div>
-                <div className={styles.advantagesBottom}>
-                    <div className={styles.advantages}>
-                        <Image className={styles.icons} src={chat} alt="chat-image"/>
-                        <h3 className={styles.title}>Прямая связь с опытными программистами</h3>
-                        <p className={styles.descriptionFifth}>Задавайте вопросы и отправляйте свой код на проверку</p>
-                    </div>
-                    <div className={styles.advantages}>
-                        <Image className={styles.icons} src={coin} alt="coin-image"/>
-                        <h3 className={styles.title}>Оплата небольшими частями</h3>
-                        <p className={styles.descriptionSixth}>Платите только за тот материал, который сейчас изучаете, а не за весь курс сразу</p>
-                    </div>
-                    <div className={styles.advantages}>
-                        <Image className={styles.icons} src={phoneCard} alt="phone-card-image"/>
-                        <h3 className={styles.title}>Быстрый и легкий возврат</h3>
-                        <p className={styles.descriptionSeventh}>Если передумаете учиться – вернем деньги за 3 рабочих дня</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.knowledgeWrapper}>
+			<div className={styles.objects}>
+				<h2 className={styles.whatKnowledge}>Какими знаниями вы овладеете:</h2>
+				<div className={styles.baseKnowledge}>
+					<Image
+						src={programmer}
+						className={styles.programmerImage}
+						alt='programmer-image'
+					/>
+					<div className={styles.bKText}>
+						<div className={styles.Row}>
+							<div className={styles.progLanguage}>
+								Деньги и денежное обращение
+							</div>
+							<div className={styles.networks}>Налоги</div>
+						</div>
+						<div className={styles.Row}>
+							<div className={styles.bd}>Инвестиции</div>
+							<div className={styles.frameworks}>Цифровой банкинг</div>
+						</div>
+						<div className={styles.Row}>
+							<div className={styles.testing}>Кредиты и депозиты</div>
+							<div className={styles.docker}>Фонды</div>
+							<div className={styles.git}>Риски</div>
+						</div>
+						<p className={styles.minKnowledge}>
+							Это необходимый минимум для грамотного управления финансами
+						</p>
+					</div>
+				</div>
+				<h2 className={styles.ourEducation}>
+					Обучение у нас – это удобно и результативно
+				</h2>
+				<div className={styles.advantagesTop}>
+					<div className={styles.advantages}>
+						<Image
+							className={styles.icons}
+							src={codeTerminal}
+							alt='terminal-image'
+						/>
+						<h3 className={styles.title}>Большое количество практики</h3>
+						<p className={styles.descriptionFirst}>
+							Более 100 самостоятельных заданий и 10 полноценных практических
+							занятий
+						</p>
+					</div>
+					<div className={styles.advantages}>
+						<Image className={styles.icons} src={sphere} alt='sphere-image' />
+						<h3 className={styles.title}>Современные методики обучения</h3>
+						<p className={styles.descriptionSecond}>
+							Спиральное обучение: погружаемся в материал постепенно, виток за
+							витком
+						</p>
+					</div>
+					<div className={styles.advantages}>
+						<Image
+							className={styles.icons}
+							src={monitorLines}
+							alt='monitor-lines-image'
+						/>
+						<h3 className={styles.title}>Простое и понятное изложение</h3>
+						<p className={styles.descriptionThird}>
+							Вместо заумных терминов – примеры из реального мира
+						</p>
+					</div>
+					<div className={styles.advantages}>
+						<Image
+							className={styles.iconsCalendar}
+							src={calendar}
+							alt='calendar-image'
+						/>
+						<h3 className={styles.title}>Гибкий график занятий</h3>
+						<p className={styles.descriptionFourth}>
+							Учитесь в любое время в удобном для вас темпе
+						</p>
+					</div>
+				</div>
+				<div className={styles.advantagesBottom}>
+					<div className={styles.advantages}>
+						<Image className={styles.icons} src={chat} alt='chat-image' />
+						<h3 className={styles.title}>Прямая связь с опытными педагогами</h3>
+						<p className={styles.descriptionFifth}>
+							Задавайте вопросы и отправляйте свои ответы на проверку
+						</p>
+					</div>
+					<div className={styles.advantages}>
+						<Image className={styles.icons} src={coin} alt='coin-image' />
+						<h3 className={styles.title}>Все абсолютно бесплатно</h3>
+						<p className={styles.descriptionSixth}>
+							Мы не взимаем оплаты ни на одном из этапов обучения
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
